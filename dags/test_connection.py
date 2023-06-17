fb9ffc9b070dd84997c1346b5c8bc0173fbfe508
L3ni0 <dominiklenio@gmail.com>
2023-06-17 13:10:23 +0200

    testing connestion with azure sql server

--- dags/test_connection.py
+++ dags/test_connection.py
@@ -1,4 +1,20 @@
-import os
+import pypyodbc as odbc
+import pandas as pd
 
-if os.path.isfile('./curated/date.csv'):
-            print('if')
+
+server = 'loaded.database.windows.net'
+database = 'airport-to-analyze'
+connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:loaded.database.windows.net,1433;Database=airport-to-analyze;Uid=domini;Pwd={Dziekanchuj!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+
+conn = odbc.connect(connection_string)
+
+
+cursor = conn.cursor()
+
+statement = '''
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES'''
+
+cursor.execute(statement)
+
+print(cursor.fetchall())
